c3d8f7b0bb2b603b60448eeab3ef7a5f5468b018
JOÃO GUILHERME FERNANDES <joaoguilherme89@hotmail.com.br>
2022-11-05 15:31:15 -0300

    Learning NestJs

---
+++ src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { ScheduleModule } from '@nestjs/schedule';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { join } from 'path';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TweetsModule } from './tweets/tweets.module';
+import { TweetsCountService } from './tweets/tweets-count/tweets-count.service';
+
+@Module({
+  imports: [
+    ScheduleModule.forRoot(),
+    SequelizeModule.forRoot({
+      dialect: 'sqlite',
+      host: join(__dirname, 'database.sqlite'),
+      autoLoadModels: true,
+      synchronize: true,
+    }),
+    TweetsModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
